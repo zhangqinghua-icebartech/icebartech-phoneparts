63387dc5b07bc17d8cabf9616f630a6dc2a0c94f
liuao <aliu95@qq.com>
2020-10-10 17:16:02 +0800

    init

--- icebartech-base/src/main/java/com/icebartech/base/manager/service/RoleService.java
+++
@@ -1,8 +0,0 @@
-package com.icebartech.base.manager.service;
-
-import com.icebartech.base.manager.dto.Role;
-import com.icebartech.base.manager.po.SysRole;
-import com.icebartech.core.modules.BaseService;
-
-public interface RoleService extends BaseService<Role, SysRole> {
-}
--- icebartech-core/src/main/java/com/icebartech/core/vo/RespDate.java
+++
@@ -1,8 +0,0 @@
-package com.icebartech.core.vo;
-
-import io.swagger.annotations.ApiModel;
-
-@ApiModel(value = "RespDate", description = "返回的json数据对象")
-public class RespDate<E> extends RespJson<BaseData<E>> {
-
-}

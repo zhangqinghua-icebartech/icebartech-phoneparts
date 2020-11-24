fdc5bcf8bb819b25c71cecb3d2aad570b763a2a0
zhangqh <123456go>
2020-11-24 15:05:32 +0800

    Mod: 修复序列号登录Bug，优化切割统计接口。

--- icebartech-starter/src/main/java/com/icebartech/phoneparts/product/po/Product.java
+++ icebartech-starter/src/main/java/com/icebartech/phoneparts/product/po/Product.java
@@ -52,6 +52,7 @@ public class Product extends BasePo {
     @Column(columnDefinition = "varchar(225) NOT NULL COMMENT '展示图'")
     private String coverIcon;
 
+
     @ApiModelProperty(value = "详情图",example = "详情图")
     @Column(columnDefinition = "varchar(225) NOT NULL COMMENT '详情图'")
     private String detailIcon;

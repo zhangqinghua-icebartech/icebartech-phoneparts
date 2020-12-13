14bdaab458e553e24e480330e3c44c2185e9c14a
zhangqh <123456go>
2020-12-13 17:32:06 +0800

    Fix: 设备管理还原一下 按照  ：一级分类名+日期（如20201212）+后面规则忘记了应该是随机      兑换码格式：一级分类名称+部分日期（如：201212，2020只要后面的两位数）+后面规则忘记了应该是随机

--- icebartech-starter/src/main/java/com/icebartech/phoneparts/util/ProduceCodeUtil.java
+++ icebartech-starter/src/main/java/com/icebartech/phoneparts/util/ProduceCodeUtil.java
@@ -10,13 +10,12 @@ import java.util.Random;
  */
 public class ProduceCodeUtil {
 
-
     /**
      * 生产兑换码
      * @return 兑换码
      */
     public static String findRedeemCode(){
-        return findDateNum() + findChar(6);
+        return findDateNum("yyMMdd") + findChar(6);
     }
 
 
@@ -26,7 +25,7 @@ public class ProduceCodeUtil {
      * @return 序列号
      */
     public static String findSerialNum(String title){
-        return title + findDateNum("yyyyMM") + findChar(6);
+        return title + findDateNum() + findChar(6);
     }
 
     /**

ed0d6dda9b8390ae451a586a87f88ca51b418ff4
zhangqinghua <the.patron.saint.of.science@gmail.com>
2021-02-28 23:13:24 +0800

    Fix：修复无法设置准备类型Bug。

--- icebartech-starter/src/main/java/com/icebartech/phoneparts/system/param/SysDeviceInsertParam.java
+++ icebartech-starter/src/main/java/com/icebartech/phoneparts/system/param/SysDeviceInsertParam.java
@@ -11,22 +11,25 @@ import lombok.Data;
 @Data
 public class SysDeviceInsertParam {
 
-    @ApiModelProperty(value = "设备名称",example = "设备名称")
+    @ApiModelProperty(value = "设备类型 0-手机版 1-竖屏版 2-横屏版", example = "0")
+    private Integer type;
+
+    @ApiModelProperty(value = "设备名称", example = "设备名称")
     private String name;
 
-    @ApiModelProperty(value = "代理商id",example = "代理商id")
+    @ApiModelProperty(value = "代理商id", example = "代理商id")
     private Long agentId;
 
-    @ApiModelProperty(value = "详情图",example = "详情图")
+    @ApiModelProperty(value = "详情图", example = "详情图")
     private String detailIcon;
 
-    @ApiModelProperty(value = "文件",example = "文件")
+    @ApiModelProperty(value = "文件", example = "文件")
     private String file;
 
-    @ApiModelProperty(value = "文件名称",example = "文件名称")
+    @ApiModelProperty(value = "文件名称", example = "文件名称")
     private String fileName;
 
-    @ApiModelProperty(value = "使用状态",example = "使用状态")
+    @ApiModelProperty(value = "使用状态", example = "使用状态")
     private ChooseType state;
 
 }
